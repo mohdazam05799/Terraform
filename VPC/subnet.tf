b221170d5b975a1e3b96a939e7a40f7cd45456a6
Mohd Azam <mohdazam05799@gmail.com>
2023-01-05 21:46:17 +0530

    Revert "EC2 Terraform"
    This reverts commit 6eb81037776984ec2426125ad9e11d3c7b62c670.

--- VPC/subnet.tf
+++ VPC/subnet.tf
@@ -2,6 +2,8 @@ resource "aws_subnet" "public" {
   count             = 2
   vpc_id            = aws_vpc.Test-VPC.id
   availability_zone = data.aws_availability_zones.available.names[count.index]
+  map_public_ip_on_launch = true
+  cidr_block              = cidrsubnet(var.cidr, 2, count.index)
 
   tags = {
     "Name" = "${var.environment}-vpc-public-subnet-${count.index + 1}"
@@ -16,6 +18,7 @@ resource "aws_subnet" "public" {
 resource "aws_subnet" "private" {
   vpc_id            = aws_vpc.Test-VPC.id
   count             = 2
+  cidr_block        = cidrsubnet(var.cidr, 2, format("%d", count.index + 2))
   availability_zone = data.aws_availability_zones.available.names[count.index]
   tags = {
     "Name" = "${var.environment}-vpc-private-subnet-${count.index + 1}"
